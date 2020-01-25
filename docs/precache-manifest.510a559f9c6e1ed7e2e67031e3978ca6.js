726b034e83164bf6d67a690853433cd20770a726
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-01-25 17:27:42 +0900

    configurating build
    Signed-off-by: Jongkeun Choi <jongkeun.ch@gmail.com>

---
+++ docs/precache-manifest.510a559f9c6e1ed7e2e67031e3978ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be10a62152ff375d81446daf3ec349b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57684ed319b429079125",
+    "url": "/static/css/main.d5ffbf88.chunk.css"
+  },
+  {
+    "revision": "f517bd7daa6b07dc54d7",
+    "url": "/static/js/2.9997a948.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9997a948.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57684ed319b429079125",
+    "url": "/static/js/main.b21ebf25.chunk.js"
+  },
+  {
+    "revision": "cf8f4028165d4d4b3e3c",
+    "url": "/static/js/runtime-main.583438c4.js"
+  }
+]);
